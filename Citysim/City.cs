285442c15f4767cca472d465c96949dd436eff4b
Mitchfizz05 <me@mitchfizz05.net>
2016-04-23 17:06:22 +0930

    Add city power and water variables.

--- Citysim/City.cs
+++ Citysim/City.cs
@@ -10,6 +10,16 @@ namespace Citysim
         public int cash = 10000; // $10,000 starting cash
 
         /// <summary>
+        /// MW (mega watts) of electricity available to the city.
+        /// </summary>
+        public int power = 0;
+
+        /// <summary>
+        /// ML (mega litres) of water available to the city.
+        /// </summary>
+        public int water = 0;
+
+        /// <summary>
         /// The world. Needs generation.
         /// </summary>
         public World world = new World();
