1a92f1267518889c09b6a9c515216bfa3cf9c94a
Mitchfizz05 <me@mitchfizz05.net>
2016-04-23 18:55:28 +0930

    Add custom tile renderer interface.

---
+++ Citysim/Map/Tiles/ITileCustomRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Citysim.Map.Tiles
+{
+    interface ITileCustomRenderer
+    {
+        /// <summary>
+        /// Where the tile can do the custom rendering.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        /// <param name="game"></param>
+        /// <param name="position"></param>
+        void Render(SpriteBatch spriteBatch, Citysim game, Vector2 position);
+    }
+}
