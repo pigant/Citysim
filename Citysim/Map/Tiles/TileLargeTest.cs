5715aeb0ae3cb6214b7a3797fc9f1ba02785449c
Mitchfizz05 <me@mitchfizz05.net>
2016-04-27 00:33:09 +0930

    Add large tile test.

---
+++ Citysim/Map/Tiles/TileLargeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Citysim.Map.Tiles
+{
+    class TileLargeTest : Tile
+    {
+        public TileLargeTest(int id) : base(id)
+        {
+            this.SetTextureName("tiles/64x64test");
+            this.tileSize = new Point(2, 2);
+        }
+    }
+}
