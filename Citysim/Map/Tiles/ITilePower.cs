e634e81a132a0dbf1f8c9e7e566aa6bf23cea092
Mitchfizz05 <me@mitchfizz05.net>
2016-04-27 03:00:47 +0930

    Add power provider interface.

---
+++ Citysim/Map/Tiles/ITilePower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citysim.Map.Tiles
+{
+    interface ITilePower
+    {
+        /// <summary>
+        /// Produce or consume power here.
+        /// Negative number for consuption, positive number for production.
+        /// Measured in MW.
+        /// </summary>
+        /// <returns>Power production/consuption</returns>
+        int EnergyUsage();
+    }
+}
